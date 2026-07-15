bb7aa66617a595851e5be285a6dccc2566b1f161
Hannaneh Babaeeyan <Hannaneh.bab2007@gmail.com>
2026-07-15 17:58:05 +0330

    Complete Course model and admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "professor", "user", "created_at")
+    search_fields = ("name", "professor")
+    list_filter = ("created_at",)
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Course(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="courses"
+    )
+
+    name = models.CharField(max_length=100)
+
+    professor = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
