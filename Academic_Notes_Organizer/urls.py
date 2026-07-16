d522c1ab9b725827fd18cc8934551786de8a07c3
Hannaneh Babaeeyan <Hannaneh.bab2007@gmail.com>
2026-07-16 12:08:09 +0330

    Add course creation feature

--- Academic_Notes_Organizer/urls.py
+++ Academic_Notes_Organizer/urls.py
@@ -1,8 +1,9 @@
-
 from django.contrib import admin
-from django.urls import path ,include
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include("accounts.urls")),
+    path("admin/", admin.site.urls),
+    path("", include("accounts.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("courses/", include("courses.urls")),
 ]
