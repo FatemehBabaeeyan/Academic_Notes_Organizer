8b45dcaf5603adc6da2564776b22b2b9f02de1dc
Hannaneh Babaeeyan <Hannaneh.bab2007@gmail.com>
2026-07-17 11:26:21 +0330

    Complete courses CRUD

--- courses/urls.py
+++ courses/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.course_list, name="course_list"),
     path("create/" , views.course_create , name = "course_create"),
+    path("update/<int:pk>/", views.course_update, name="course_update"),
+    path("delete/<int:pk>/", views.course_delete, name="course_delete"),
 ]
--- courses/views.py
+++ courses/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render ,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Course
 from .forms import CourseForm
-
+from django.shortcuts import get_object_or_404
 
 @login_required
 def course_list(request):
@@ -39,4 +39,46 @@ def course_create(request):
         request,
         "courses/course_create.html",
         {"form": form},
+    )
+
+@login_required
+def course_update(request, pk):
+    course = get_object_or_404(
+        Course,
+        pk=pk,
+        user=request.user
+    )
+
+    if request.method == "POST":
+        form = CourseForm(request.POST, instance=course)
+
+        if form.is_valid():
+            form.save()
+            return redirect("course_list")
+
+    else:
+        form = CourseForm(instance=course)
+
+    return render(
+        request,
+        "courses/course_update.html",
+        {"form": form}
+    )
+@login_required
+def course_delete(request, pk):
+
+    course = get_object_or_404(
+        Course,
+        pk=pk,
+        user=request.user
+    )
+
+    if request.method == "POST":
+        course.delete()
+        return redirect("course_list")
+
+    return render(
+        request,
+        "courses/course_delete.html",
+        {"course": course}
     )
